0749a58c2e5f3a9cddca375c1a4212c1d53e806e
irgidp <irgijak@gmail.com>
2020-10-15 22:29:59 +0700

    Latihan 16

---
+++ PBOIF210119059Latihan16Penugasan.java
@@ -0,0 +1,54 @@
+/* 
+* @author
+ * NAMA        : Irgi Dwiputra
+ * KELAS       : IF-2
+ * NIM         : 10119059
+ * Deskripsi   : Program ini berisi program untuk menampilkan sintaks penugasan
+ */
+package pboif2.pkg10119059.latihan16.penugasan;
+
+public class PBOIF210119059Latihan16Penugasan {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a =  " + a);
+        System.out.println("Nilai b =  " + b);
+        System.out.println("Nilai c =  " + c +"\n");
+        
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c += a;
+        System.out.println("c += a = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a = " + c);
+        
+        a = 10;
+        c = 15;
+        c %= a;
+        System.out.println("c %= a = " + c);
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= a = " + c);
+        c &= a;
+        System.out.println("c &= a = " + c);
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        c |= a;
+        System.out.println("c |= a = " + c);
+    }
+    
+}
